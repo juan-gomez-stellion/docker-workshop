22df859fd5b6badf98a11fc0c0d0121bc0b373fa
EC2 Default User <ec2-user@ip-172-31-33-69.ec2.internal>
2023-11-21 23:48:49 +0000

    Agrego opcion para crear tablas de dynamo

--- dynamodb.py
+++ dynamodb.py
@@ -6,4 +6,42 @@ def listar_tablas():
     for table in dynamodb.tables.all():
         print(table.name)
         
-listar_tablas() 
+#crear funcion para crear tabla de dynamodb con capacidad bajo demanda
+def crear_tabla_dynamodb(nombre_tabla):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.create_table(
+        TableName=nombre_tabla,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'  #Partition key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        BillingMode='PAY_PER_REQUEST'
+    )
+    print("Tabla creada exitosamente")
+        
+        
+opcion = 1
+
+while opcion != 0:
+    print("Ingrese la opción que desea")
+    print("1. Listar tablas de dynamodb de la cuenta")
+    print("2. Crear nueva tabla")
+    print("0. Salir")
+    opcion = int(input())
+    if opcion == 1:
+        listar_tablas()  
+    elif opcion == 2:
+        nombre_tabla = input("Ingrese el nombre de la tabla")
+        crear_tabla_dynamodb(nombre_tabla)
+    elif opcion == 0:
+        print("Hasta luego")
+    else:
+        print("Opción no válida")
